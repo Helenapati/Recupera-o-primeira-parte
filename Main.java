8a2dd47d247670a67c0fb8790ace28bff8dd7adc
Helenapati <88396481+Helenapati@users.noreply.github.com>
2021-09-24 09:07:31 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,46 @@
+
+public class Produto { 
+Scanner calc = New Scanner (System.in)
+private String nome; 
+private double preco; 
+private int  uantidadeEstoque; 
+private double precoCusto:
+private double precoVenda;
+public Produto(String nome, double preco, int quantidade, double custo, double venda) { 
+this.nome = nome; 
+this.preco = preco; 
+this.quantidadeEstoque = quantidade;
+this.precoCusto = custo;
+this.precoVenda = valor;
+} 
+public String getNome() { 
+return this.nome; 
+} 
+public double getPreco() { 
+return this.preco; 
+} 
+public int getQuantidade() { 
+return this.quantidadeEstoque;
+} 
+public double getCusto(){
+return this.precoCusto;
+}
+public void setCusto(int Custo) {
+this.precoCusto = Custo;
+}
+public double getValor(){
+return this.precoVenda;
+}
+public void setValor(double Valor) {
+this.precoVenda = Valor;
+} 
+
+float lucro; 
+
+Calclucro(){ 
+
+Lucro = precoValor - precoCusto; 
+
+System.out.println ("Seu lucro é:" + lucro); 
+
+}
